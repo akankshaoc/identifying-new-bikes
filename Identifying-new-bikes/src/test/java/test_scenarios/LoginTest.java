be4c5097266f88dfc932b3f91b3c373350f0763a
Akanksha Sharma <Akanksha.Sharma3@cognizant.com>
2024-03-05 13:58:47 +0530

    bug fixes

--- Identifying-new-bikes/src/test/java/test_scenarios/LoginTest.java
+++ Identifying-new-bikes/src/test/java/test_scenarios/LoginTest.java
@@ -8,54 +8,62 @@ import org.testng.annotations.Test;
 import common.AppTest;
 import page_objects.LoginPage;
 
-public class LoginTest extends AppTest{
+public class LoginTest extends AppTest {
 	LoginPage loginPage;
-	
+
 	@BeforeClass
-	public void initialise() throws InterruptedException { 
-		loginPage = homePage.launchLoginPage();		
+	public void initialise() throws InterruptedException {
+		loginPage = homePage.launchLoginPage();
 	}
-	
+
 	@Test(dataProvider = "email", dataProviderClass = data_provider.LoginCredentials.class)
 	public void positiveEmailLoginTest(String email) throws InterruptedException {
 		loginPage.fillSignInForm(email);
+		// wait for ui changes to take place
+		Thread.sleep(2000);
 		boolean signInAccepted = loginPage.singInAccepted();
-		if(signInAccepted) {
+		if (signInAccepted) {
 			driver.navigate().back();
 		}
 		Assert.assertTrue(signInAccepted);
 	}
-	
+
 	@Test(dataProvider = "email", dataProviderClass = data_provider.LoginCredentials.class)
 	public void negativeEmailLoginTest(String email) throws InterruptedException {
 		loginPage.fillSignInForm(email);
+		// wait for ui changes to take place
+		Thread.sleep(2000);
 		boolean signInAccepted = loginPage.singInAccepted();
-		if(signInAccepted) {
+		if (signInAccepted) {
 			driver.navigate().back();
 		}
 		Assert.assertFalse(signInAccepted);
 	}
-	
+
 	@Test(dataProvider = "phone_number", dataProviderClass = data_provider.LoginCredentials.class)
 	public void positivePhoneNumberLoginTest(String phno) throws InterruptedException {
 		loginPage.fillSignInForm(phno);
+		// wait for ui changes to take place
+		Thread.sleep(2000);
 		boolean signInAccepted = loginPage.singInAccepted();
-		if(signInAccepted) {
+		if (signInAccepted) {
 			driver.navigate().back();
 		}
 		Assert.assertTrue(signInAccepted);
 	}
-	
+
 	@Test(dataProvider = "phone_number", dataProviderClass = data_provider.LoginCredentials.class)
 	public void negativePhoneNumberLoginTest(String phno) throws InterruptedException {
 		loginPage.fillSignInForm(phno);
+		// wait for ui changes to take place
+		Thread.sleep(2000);
 		boolean signInAccepted = loginPage.singInAccepted();
-		if(signInAccepted) {
+		if (signInAccepted) {
 			driver.navigate().back();
 		}
 		Assert.assertFalse(signInAccepted);
 	}
-	
+
 	@AfterClass
 	public void naturalise() {
 		driver.close();
